4533fc181f34199be76e3213331b11552605511b
Chetan12-rgb <142248955+Chetan12-rgb@users.noreply.github.com>
2023-10-26 17:00:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let menuIcon= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+
+menuIcon.onclick =() =>{
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+};
+
+// menuIcon.classList.remove('fa-regular fa-x');
+// navbar.classList.remove('active');
